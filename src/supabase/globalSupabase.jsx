dc208bc2182d3b5491b264b2f96bce38828d835c
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-03-08 22:24:21 -0500

    refactor globalSupabase to simplify id extraction from user session

--- src/supabase/globalSupabase.jsx
+++ src/supabase/globalSupabase.jsx
@@ -6,7 +6,20 @@ export const ObtenerIdAuthSupabase = async() => {
 
     if (session) {
         const { user } = session;
-        const { idAuthSupabase } = user.id;
+        const idAuthSupabase = user.id;
         return idAuthSupabase;        
     }
 }
+
+// export async function getSessionId() {
+//     const { data: { session } } = await supabase.auth.getSession();
+
+//     if (session) {
+//         const { user } = session;
+//         const idAuthSupabase = user.id;
+//         return idAuthSupabase;
+//     }
+
+//     return null;
+
+// }
