3207acf02ac870aed8954de1999fdd1a3c6b6e92
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-30 21:30:52 -0500

    refactor Header component for improved structure and event handling

--- src/components/organismos/Header.jsx
+++ src/components/organismos/Header.jsx
@@ -1,13 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { ContentHeader } from '../atomos/ContentHeader';
-import { DataUser } from '../organismos/DataUser';
+import { ContentHeader } from "../atomos/ContentHeader";
+import { DataUser } from "../organismos/DataUser";
 
 export function Header({ stateConfig }) {
-    return (
-        <ContentHeader>
-            <DataUser stateConfig={stateConfig}/>            
-        </ContentHeader>
-    );
+  return (
+    <ContentHeader>
+      <div onClick={(e) => e.stopPropagation()}>
+        <DataUser stateConfig={stateConfig} />
+      </div>
+    </ContentHeader>
+  );
 }
-
